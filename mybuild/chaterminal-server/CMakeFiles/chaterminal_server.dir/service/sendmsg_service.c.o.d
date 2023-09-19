7ca076ffe40fd0ef06b90c7e68b43c234fb5c2f8
Shibo Ai <shibo@James-Webb>
2023-09-19 15:06:44 -0700

    Removed unwanted files and folders

--- mybuild/chaterminal-server/CMakeFiles/chaterminal_server.dir/service/sendmsg_service.c.o.d
+++
@@ -1,4 +0,0 @@
-chaterminal-server/CMakeFiles/chaterminal_server.dir/service/sendmsg_service.c.o: \
- /mnt/c/Users/asnic/CLionProjects/chaterminal/chaterminal-server/service/sendmsg_service.c \
- /usr/include/stdc-predef.h \
- /mnt/c/Users/asnic/CLionProjects/chaterminal/chaterminal-server/include/sendmsg_service.h
