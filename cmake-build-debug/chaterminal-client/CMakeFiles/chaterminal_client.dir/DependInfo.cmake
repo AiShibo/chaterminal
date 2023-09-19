9f361d375e621aebcdc7abe71b36c4f56f4d3fd6
AiShibo <shibo.ai@outlook.com>
2023-09-19 15:01:55 -0700

    bug fix

---
+++ cmake-build-debug/chaterminal-client/CMakeFiles/chaterminal_client.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/asnic/CLionProjects/chaterminal/chaterminal-client/callback/auth_cb.c" "chaterminal-client/CMakeFiles/chaterminal_client.dir/callback/auth_cb.c.o" "gcc" "chaterminal-client/CMakeFiles/chaterminal_client.dir/callback/auth_cb.c.o.d"
+  "/mnt/c/Users/asnic/CLionProjects/chaterminal/chaterminal-client/callback/chat_cb.c" "chaterminal-client/CMakeFiles/chaterminal_client.dir/callback/chat_cb.c.o" "gcc" "chaterminal-client/CMakeFiles/chaterminal_client.dir/callback/chat_cb.c.o.d"
+  "/mnt/c/Users/asnic/CLionProjects/chaterminal/chaterminal-client/callback/event_cb.c" "chaterminal-client/CMakeFiles/chaterminal_client.dir/callback/event_cb.c.o" "gcc" "chaterminal-client/CMakeFiles/chaterminal_client.dir/callback/event_cb.c.o.d"
+  "/mnt/c/Users/asnic/CLionProjects/chaterminal/chaterminal-client/callback/user_cb.c" "chaterminal-client/CMakeFiles/chaterminal_client.dir/callback/user_cb.c.o" "gcc" "chaterminal-client/CMakeFiles/chaterminal_client.dir/callback/user_cb.c.o.d"
+  "/mnt/c/Users/asnic/CLionProjects/chaterminal/chaterminal-client/main.c" "chaterminal-client/CMakeFiles/chaterminal_client.dir/main.c.o" "gcc" "chaterminal-client/CMakeFiles/chaterminal_client.dir/main.c.o.d"
+  "/mnt/c/Users/asnic/CLionProjects/chaterminal/chaterminal-client/service/server_connect_service.c" "chaterminal-client/CMakeFiles/chaterminal_client.dir/service/server_connect_service.c.o" "gcc" "chaterminal-client/CMakeFiles/chaterminal_client.dir/service/server_connect_service.c.o.d"
+  "/mnt/c/Users/asnic/CLionProjects/chaterminal/chaterminal-client/util/get_serv_msg.c" "chaterminal-client/CMakeFiles/chaterminal_client.dir/util/get_serv_msg.c.o" "gcc" "chaterminal-client/CMakeFiles/chaterminal_client.dir/util/get_serv_msg.c.o.d"
+  "/mnt/c/Users/asnic/CLionProjects/chaterminal/chaterminal-client/util/process_response.c" "chaterminal-client/CMakeFiles/chaterminal_client.dir/util/process_response.c.o" "gcc" "chaterminal-client/CMakeFiles/chaterminal_client.dir/util/process_response.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/chaterminal-server/CMakeFiles/chaterminal_server.dir/service/modify_service.c.o.d
@@ -0,0 +1,4 @@
+chaterminal-server/CMakeFiles/chaterminal_server.dir/service/modify_service.c.o: \
+ /mnt/c/Users/asnic/CLionProjects/chaterminal/chaterminal-server/service/modify_service.c \
+ /usr/include/stdc-predef.h \
+ /mnt/c/Users/asnic/CLionProjects/chaterminal/chaterminal-server/include/modify_service.h
