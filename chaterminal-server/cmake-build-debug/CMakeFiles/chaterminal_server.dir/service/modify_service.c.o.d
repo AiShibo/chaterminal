7538d62a89444de9fe717ba894baa526a6524b52
AiShibo <shibo.ai@outlook.com>
2023-09-19 13:42:18 -0700

    Untrack files and update .gitignore

--- chaterminal-server/cmake-build-debug/CMakeFiles/chaterminal_server.dir/service/modify_service.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/chaterminal_server.dir/service/modify_service.c.o: \
- /mnt/c/Users/asnic/CLionProjects/chaterminal-server/service/modify_service.c \
- /usr/include/stdc-predef.h \
- /mnt/c/Users/asnic/CLionProjects/chaterminal-server/include/modify_service.h
--- chaterminal-server/cmake-build-debug/CMakeFiles/chaterminal_server.dir/service/sendmsg_service.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/chaterminal_server.dir/service/sendmsg_service.c.o: \
- /mnt/c/Users/asnic/CLionProjects/chaterminal-server/service/sendmsg_service.c \
- /usr/include/stdc-predef.h \
- /mnt/c/Users/asnic/CLionProjects/chaterminal-server/include/sendmsg_service.h
